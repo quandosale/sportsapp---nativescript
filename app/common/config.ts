b8f7d029726b1b48e9c99f011bdbe8b85e618702
a0af <a0af@yandex.com>
2017-06-06 00:51:37 +0800

    integrate ota

--- app/common/config.ts
+++ app/common/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
-    SERVER_URL: 'http://192.168.3.4:3002',
-    // SERVER_URL: 'http://13.112.160.235',
+    // SERVER_URL: 'http://192.168.3.4:3002',
+    SERVER_URL: 'http://13.112.160.235',
     timeout: 6000,
     COOKIE_FILE: 'calm_tmp.dat'
 };
--- j2.bat
+++ j2.bat
@@ -1,2 +1,4 @@
 @echo Ota Dfu module
-@copy G:\!work\OtaTester\app\build\outputs\aar\app-release.aar F:\SportsApp\node_modules\nativescript-sportsotadfu\platforms\android\app-release.aar
+@copy G:\!work\OtaTester\otadfu\build\outputs\aar\otadfu-release.aar G:\!work\nativescript\SportsApp\node_modules\nativescript-sportsotadfu\platforms\android\otadfu-release.aar
+
+@copy G:\!work\OtaTester\common\build\outputs\aar\common-release.aar G:\!work\nativescript\SportsApp\node_modules\nativescript-sportsotadfu\platforms\android\common-release.aar
